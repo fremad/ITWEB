9275862fbda3d3d40dffbeabb6294ed99050c818
Lars <larshjerrild@hotmail.com>
2017-10-05 20:59:57 +0200

    angular serving

--- HandInOne/myapp/app.js
+++ HandInOne/myapp/app.js
@@ -14,8 +14,7 @@ var routesApi = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname,'app_server' ,'views'));
-app.set('view engine', 'pug');
+app.use( express.static(__dirname + '/dist' ) );
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
